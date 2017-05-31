a03a5ce1d22bd7b5e3b5a146d01ced05bc28a55e
jimssell <morales_jimssell2000@yahoo.com>
2017-05-31 21:13:10 +0800

    add files

---
+++ seldin/App_Start/BundleConfig.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace seldin
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Assets/js/jquery.js",
+                      "~/Assets/js/bootstrap.min.js",
+                      "~/Assets/js/jquery.mixitup.js",
+                      "~/Assets/js/owl.carousel.js",
+                      "~/Assets/js/wow.min.js",
+                      "~/Assets/js/main.js",
+                      "~/Assets/js/jquery.magnific-popup.js" ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Assets/bootstrap.min.css",
+                      "~/Assets/css/font-awesome.min.css",
+                      "~/Assets/css/animate.css",
+                      "~/Content/bootstrap.min.css",
+                      "~/Assets/css/magnific-popup.css",
+                      "~/Assets/css/style.css",
+                      "~/Assets/css/responsive.css",
+                      "~/Assets/css/owl.carousel.css"));
+        }
+    }
+}
---
+++ seldin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(seldin.Startup))]
+namespace seldin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
