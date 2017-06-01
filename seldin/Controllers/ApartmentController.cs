44af17bd599f6f35c654a0981cd04373ec7f5c6f
johnko12 <johncarlo@365connect.com>
2017-06-01 22:07:12 +0800

    Add ViewApartment
    Add ViewApartment

--- seldin/Controllers/ApartmentController.cs
+++ seldin/Controllers/ApartmentController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using seldin.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,17 @@ namespace seldin.Controllers
         {
             return View();
         }
+
+        public ActionResult ViewApartment(int id)
+        {
+            var apartment = new Apartment();
+            var apartments = apartment.GetApartments();
+            var models = apartments.Where(x => x.Id == id).FirstOrDefault();
+
+            ViewBag.Models = models;
+
+            return View(models);
+        }
+
     }
 }
