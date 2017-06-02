9a93f1ef9caa43eea1c9033199ff0fff08b16438
jimssell <morales_jimssell2000@yahoo.com>
2017-06-03 07:17:21 +0800

    upate the theme color

---
+++ seldin/Controllers/AssociatesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace seldin.Controllers
+{
+    public class AssociatesController : Controller
+    {
+        // GET: Associates
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
